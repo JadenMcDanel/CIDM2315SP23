fc16da5effc49e7a28adb72b7bbbdcd3f52fbcc4
JadenMcDanel <143651884+JadenMcDanel@users.noreply.github.com>
2023-08-30 20:09:14 -0500

    Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         double X = 2.5;
         double Y = 3.3;
         double Z = 4 * X * X + 3 * Y;
-        Console.WriteLine($"X = {X}, Y = {Y}, ");
-        Console.WriteLine($"The value of Z is: {Z}, ");
+        Console.WriteLine($"X = {X}, Y = {Y} ");
+        Console.WriteLine($"The value of Z is: {Z} ");
     }
 }
