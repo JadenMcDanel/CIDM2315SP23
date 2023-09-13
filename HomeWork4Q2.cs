1c068b3abfa3761e7e4719414cce1961f3f832fc
JadenMcDanel <143651884+JadenMcDanel@users.noreply.github.com>
2023-09-13 18:25:52 -0500

    Add files via upload
    HomeWork4 Question 2

---
+++ HomeWork4Q2.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork4Part2;
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+        Console.WriteLine("Please insert number: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+        
+   for(int row = 0; row<n; row++){
+    for(int col = 0; col<n; col++)
+
+    {
+            Console.Write('*');
+    }
+    Console.WriteLine("");
+   }
+
+   }
+}
