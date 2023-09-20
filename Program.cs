b2d6253676e56441e63ccc6867256b9794af7fed
JadenMcDanel <143651884+JadenMcDanel@users.noreply.github.com>
2023-09-20 11:21:28 -0500

    Add files via upload
    Homework 4 Part 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework4P1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please insert first number: ");
+        int a = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Please insert second number: ");
+        int b = Convert.ToInt16(Console.ReadLine());
+    
+    if(a>b){
+        Console.WriteLine($"a = {a}; b = {b}");
+    Console.WriteLine($"The largest number is {a}");}
+    else if(b>a){
+        Console.WriteLine($"a = {a}; b = {b}");
+        Console.WriteLine($"The largest number is {b}");
+    }
+    else if(a==b){
+    Console.WriteLine($"a = {a}; b = {b}");
+    Console.WriteLine($"The numbers are equal.");
+}
+    }
+}
