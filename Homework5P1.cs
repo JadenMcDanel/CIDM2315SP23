25e99c84ab05ac2fae2184644b8e211fe1d9477e
JadenMcDanel <143651884+JadenMcDanel@users.noreply.github.com>
2023-09-27 10:25:42 -0500

    Add files via upload
    Homework 5 Question1

---
+++ Homework5P1.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework5P1;
+
+class Program
+{
+    static void Main(string[] args)
+   {
+        Console.WriteLine("Please input 2 numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int LG = Two(a,b);
+            Console.WriteLine($"a = {a}; b = {b}");
+            Console.WriteLine($"The largest number is: {LG}");
+    }
+   static int Two(int a, int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+    }
+}
