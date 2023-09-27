8fe43051b6df0bea11720a07cbcb09503f268d63
JadenMcDanel <143651884+JadenMcDanel@users.noreply.github.com>
2023-09-27 13:25:05 -0500

    Add files via upload
    Week5Question3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework5P3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            Console.WriteLine("Enter Your Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter Your Password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Enter Your Password again: ");
+            string P2 = Console.ReadLine();
+            Console.WriteLine("Enter Birthyear");
+            string B = Console.ReadLine();
+            if (int.TryParse(B, out int Birth))
+        if (password == P2 && Birth <= 2004){
+            Console.WriteLine("Account is created successfully");
+        }
+        else if(password == P2 && Birth>2004){
+            Console.WriteLine("Could not create account.");
+        }
+        else{       
+            Console.WriteLine("Wrong Password");
+        }
+            
+    }
+}
